7f59b127b9f40e27b2d52f8923f44b365f7554ab
Louis Quinnell <zioroboco@gmail.com>
2019-09-07 05:43:03 +1000

    Deploy website bucket and distribution

--- lib/stack.ts
+++ lib/stack.ts
@@ -1,7 +1,27 @@
+import { CloudFrontWebDistribution } from "@aws-cdk/aws-cloudfront"
+import { Bucket } from "@aws-cdk/aws-s3"
 import * as cdk from "@aws-cdk/core"
+import { name } from "../package.json"
 
 export class Stack extends cdk.Stack {
   constructor(app: cdk.App, id: string, props: cdk.StackProps) {
     super(app, id, props)
+
+    const bucket = new Bucket(this, "Bucket", {
+      bucketName: `${name}-website`,
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "404.html",
+    })
+
+    new CloudFrontWebDistribution(this, "Distribution", {
+      originConfigs: [
+        {
+          behaviors: [{ isDefaultBehavior: true }],
+          customOriginSource: {
+            domainName: bucket.bucketDomainName,
+          },
+        },
+      ],
+    })
   }
 }
