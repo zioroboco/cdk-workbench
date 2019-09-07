400f3acf6acdb4735a774349a454cd7882af3b6a
Louis Quinnell <zioroboco@gmail.com>
2019-09-07 14:33:17 +1000

    Use bucket static site endpoint as cloudfront origin

--- lib/stack.ts
+++ lib/stack.ts
@@ -1,14 +1,23 @@
-import { CloudFrontWebDistribution } from "@aws-cdk/aws-cloudfront"
+import {
+  CloudFrontWebDistribution,
+  OriginProtocolPolicy,
+} from "@aws-cdk/aws-cloudfront"
 import { Bucket } from "@aws-cdk/aws-s3"
 import * as cdk from "@aws-cdk/core"
 import { name } from "../package.json"
 
+const bucketName = `${name}-website`
+
 export class Stack extends cdk.Stack {
-  constructor(app: cdk.App, id: string, props: cdk.StackProps) {
+  constructor(app: cdk.App, id: string, { env, ...props }: cdk.StackProps) {
     super(app, id, props)
 
-    const bucket = new Bucket(this, "Bucket", {
-      bucketName: `${name}-website`,
+    if (!env || !env.region) {
+      throw new Error("env.region was undefined")
+    }
+
+    new Bucket(this, "Bucket", {
+      bucketName,
       websiteIndexDocument: "index.html",
       websiteErrorDocument: "404.html",
       publicReadAccess: true,
@@ -20,11 +29,13 @@ export class Stack extends cdk.Stack {
         acmCertRef:
           "arn:aws:acm:us-east-1:203242799745:certificate/97978590-a0d3-4584-aa59-daed83e6e520",
       },
+      defaultRootObject: "", // CDK defaults to `index.html`, overriding S3
       originConfigs: [
         {
           behaviors: [{ isDefaultBehavior: true }],
           customOriginSource: {
-            domainName: bucket.bucketDomainName,
+            domainName: `${bucketName}.s3-website.${env.region}.amazonaws.com`,
+            originProtocolPolicy: OriginProtocolPolicy.HTTP_ONLY,
           },
         },
       ],
